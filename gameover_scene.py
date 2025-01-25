f1055243ef72f21893fead94dab14d595031999b
Ira <prijmenko.ira@mail.ru>
2025-01-25 21:10:48 +0700

    добавлена анимация перехода (сдвиг заднего фона)

--- gameover_scene.py
+++ gameover_scene.py
@@ -9,10 +9,20 @@ class GameOverScene(Scene):
     def __init__(self, scene_manager, params=None):
         super().__init__(scene_manager, params)
         self.back_image = pygame.image.load("img/game_back.jpg")
+        self.back_image_top = 0
+        self.hidden_height = 0
 
     def on_show(self, first_time):
         self.scene_manager.kill_scene("game")
+        self.scene_manager.kill_scene("boss")
         self.time = 0
+        self.back_image_top = settings.SCREEN_HEIGHT - self.back_image.get_height()
+
+    def update(self, dt):
+        super().update(dt)
+        self.back_image_top *= 0.97
+        if self.back_image_top > 0:
+            self.back_image_top = 0
 
     def update_params(self, params):
         super().update_params(params)
@@ -30,7 +40,7 @@ class GameOverScene(Scene):
         self.score2 = font_object2.render(f"2-й игрок: {score2}", True, "green")
 
     def draw(self, screen):
-        screen.blit(self.back_image, (0, 0))
+        screen.blit(self.back_image, (0, self.back_image_top))
 
         screen.blit(self.text, (settings.SCREEN_WIDTH // 2 - self.text.get_width() // 2,
                                 settings.SCREEN_HEIGHT // 2 - self.text.get_height()))
--- trailer_scene.py
+++ trailer_scene.py
@@ -11,9 +11,15 @@ class TrailerScene(Scene):
     def __init__(self, scene_manager, params=None):
         super().__init__(scene_manager, params)
         self.back_image = pygame.image.load("img/game_back.jpg")
+        self.back_image_top = 0
+        self.hidden_height = self.back_image.get_height() - settings.SCREEN_HEIGHT
 
     def on_show(self, first_time):
         self.time = 0
+        if self.params.get("level", 1) == 1:
+            self.back_image_top = 0
+        else:
+            self.back_image_top = settings.SCREEN_HEIGHT - self.back_image.get_height()
 
     def update_params(self, params):
         super().update_params(params)
@@ -23,11 +29,14 @@ class TrailerScene(Scene):
 
     def update(self, dt):
         super().update(dt)
+        self.back_image_top -= dt * self.SCREEN_MOVE_SPEED
+        if abs(self.back_image_top) > self.hidden_height:
+            self.back_image_top = -self.hidden_height
         if self.time > self.SCENE_TIMEOUT:
             self.run_game()
 
     def draw(self, screen):
-        screen.blit(self.back_image, (0, 0))
+        screen.blit(self.back_image, (0, self.back_image_top))
         screen.blit(self.text, (settings.SCREEN_WIDTH // 2 - self.text.get_width() // 2,
                                 settings.SCREEN_HEIGHT // 2 - self.text.get_height() // 2))
 
