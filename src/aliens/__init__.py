158b5d95e5bc1c2ccc6fbc56f4157041204f651b
Ira <prijmenko.ira@mail.ru>
2025-01-30 10:16:59 +0700

    Переименование класса AlienBoss в BossAlien

--- src/aliens/__init__.py
+++ src/aliens/__init__.py
@@ -1,7 +1,7 @@
 from .alien import Alien, AlienLaserArm
 from .bonus_alien import BonusAlien
-from .boss_alien import AlienBoss
+from .boss_alien import BossAlien
 from .menu_alien import MenuAlien
 from .minion_alien import MinionAlien
 
-__all__ = [Alien, BonusAlien, AlienBoss, MenuAlien, MinionAlien, AlienLaserArm]
+__all__ = [Alien, BonusAlien, BossAlien, MenuAlien, MinionAlien, AlienLaserArm]
