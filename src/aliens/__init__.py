0da4252ec1d9427ff869ea67812e95c19a45837e
Ira <prijmenko.ira@mail.ru>
2025-02-01 21:14:31 +0700

    Исправление импорта

--- src/aliens/__init__.py
+++ src/aliens/__init__.py
@@ -1,7 +1,7 @@
 from .alien import Alien, AlienLaserArm
 from .bonus_alien import BonusAlien
 from .boss_alien import BossAlien
-from .menu_alien import MenuAlien
+from .scene_alien import SceneAlien
 from .minion_alien import MinionAlien
 
-__all__ = [Alien, BonusAlien, BossAlien, MenuAlien, MinionAlien, AlienLaserArm]
+__all__ = [Alien, BonusAlien, BossAlien, SceneAlien, MinionAlien, AlienLaserArm]
